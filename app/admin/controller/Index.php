71351f311a9cec8279572c7dd3a442ba862260b3
Kylin <kylin87@qq.com>
2019-12-14 09:52:52 +0000

    添加xadmin后台首页

---
+++ app/admin/controller/Index.php
@@ -0,0 +1,17 @@
+<?php
+declare (strict_types = 1);
+
+namespace app\admin\controller;
+
+class Index
+{
+    public function index()
+    {
+        return view();
+    }
+
+    public function welcome()
+    {
+        return view();
+    }
+}
